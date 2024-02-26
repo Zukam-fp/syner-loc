4adcae358fe55421837940935af9dd0127a0455b
zukam fodouop <zukam.fp@outlook.com>
2024-02-26 12:23:14 +0100

    Syner loc maj

---
+++ db/migrate/20240226112047_create_stuffs.rb
@@ -0,0 +1,11 @@
+class CreateStuffs < ActiveRecord::Migration[7.1]
+  def change
+    create_table :stuffs do |t|
+      t.string :name
+      t.float :price
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
