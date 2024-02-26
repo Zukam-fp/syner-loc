efb89630731fdb21151bde4a26b5b378327df0e5
Igor BAKAM <aigor.bakam@outlook.com>
2024-02-26 15:13:36 +0100

    Profile View

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     resources :stuffs, except: :index do
       resources :bookings
     end
+
+    get 'profiles', to: 'profiles#show'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
