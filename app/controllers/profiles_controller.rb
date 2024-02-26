f4f5c30384f7e887a6cece7e3b8d97c248945230
Igor BAKAM <aigor.bakam@outlook.com>
2024-02-26 17:12:19 +0100

    Profil View

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,21 @@
 class ProfilesController < ApplicationController
+  before_action :set_profile, only: %i[show]
+
+  def index
+    @profiles = Profile.all
+  end
 
   def show
   end
+
+  private
+
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:name)
+  end
+
 end
